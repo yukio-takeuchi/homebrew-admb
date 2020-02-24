e2ac4089ab9abc825e1c0ceea13468f390a9cbc6
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-24 11:01:06 +0900

    Revised to use ENV.cc and ENV.cxx for determination of CC and CXX respectively

--- admb.rb
+++ admb.rb
@@ -1,40 +1,27 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
+#
 class Admb < Formula
   desc "AD Model builder"
   homepage "http://admb-project.org"
   url "https://github.com/admb-project/admb/releases/download/admb-12.1/admb-12.1-src.zip"
   sha256 "a5541153ea55707c2a873eddc470906630aeded6d1f79c2a0a1617e8414928d0"
   # head "https://github.com/admb-project/admb.git"
-  # For testing use foled repo
+  # For testing use folked repo
   head "https://github.com/yukio-takeuchi/admb.git"
-
-  # depends_on "cmake" => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #system "make", "c++-all"  #,"-j4" # if this fails, try separate make/make install steps
     # separate make steps
     
-    system "make", "--directory=src", "CC=cc", "CXX=c++", "all"  # make c++-core
-    system "make", "--directory=contrib", "-j1", "CC=cc", "CXX=c++", "all" # make c++-contribs
+    system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all"  # make c++-core
+    system "make", "--directory=contrib", "-j1", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all" # make c++-contribs
     #system "make", "c++-dist"
     #system "make", "c++-shared"
     # Contentof  of make c++shared
-    system "make", "--directory=src", "CC=cc", "CXX=c++", "SHARED=-shared", "shared"
-    system "make", "--directory=contrib", "-j1", "CC=cc", "CXX=c++", "SHARED=-shared","shared"
+    system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared", "shared"
+    system "make", "--directory=contrib", "-j1", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared","shared"
     #####
-    system "make", "--directory=src", "CC=cc", "CXX=c++", "copy"
+    system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cc}", "copy"
     #system "make", "clang++-all" # if this fails, try separate make/make install steps
     # system "cp -r build/dist/* #{prefix}"
     #system "make", "c++-install", "INSTALL_DIR=#{prefix}/"
