1b2fd7847efe3369c0c6f48be93c78acab6209bd
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-27 20:29:04 +0900

    As of 20:30pm 2020-02-27

--- admb.rb
+++ admb.rb
@@ -7,7 +7,7 @@ class Admb < Formula
   # head "https://github.com/admb-project/admb.git"
   # For testing use folked repo
   head "https://github.com/yukio-takeuchi/admb.git"
-
+  revision 1
   def install
     # $ clang --version | grep version | sed "s/.*version \([0-9]*\.[0-9]*\).*/\1/"
     # 11.0
@@ -18,12 +18,12 @@ class Admb < Formula
     # separate make steps
     
     system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all"  # make c++-core
-    system "make", "-j1" ,"--directory=contrib",  "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all" # make c++-contribs
+    system "make", "--directory=contrib",  "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all" # make c++-contribs
     #system "make", "c++-dist"
     #system "make", "c++-shared"
     # Contentof  of make c++shared
     system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared", "shared"
-    system "make", "-j1" ,"--directory=contrib",  "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared","shared"
+    system "make", "--directory=contrib",  "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared","shared"
     #####
     system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cc}", "copy"
     #system "make", "clang++-all" 
