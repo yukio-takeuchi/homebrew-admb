c2f29b044485c87962b93dcf56bd57884b9a59fa
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2022-08-14 15:52:07 +0900

    delete diagnostics

--- admb.rb
+++ admb.rb
@@ -52,27 +52,18 @@ class Admb < Formula
     #  system "find . -type d -exec install -v -d #{prefix}/{} \\; "
     #  system "find . -type f -exec install -vC {} #{prefix}/{} \\; "
     #end
-    system "echo 55 $(pwd)"
     bin.install Dir["build/admb/bin/*"]
     lib.install Dir["build/admb/lib/*"]
     include.install Dir["build/admb/include/*"]
     prefix.install  Dir["build/admb/contrib"]
     prefix.install Dir["build/admb/examples"]
-    system "echo 61 $(pwd)"
     prefix.install Dir["build/admb/scripts"]
-    system "echo 63 $(pwd)"
     prefix.install Dir["build/admb/src"]
-    system "echo 65 $(pwd)"
     prefix.install Dir["build/admb/tests"]
-    system "echo 67 $(pwd)"
     prefix.install Dir["build/admb/utilities"]
-    system "echo 69 $(pwd)"
     prefix.install "Makefile" 
-    system "echo 70 $(pwd)"
     prefix.install Dir["*.txt"]
-    system "echo 72 $(pwd)"
     prefix.install Dir["*.md"]
-    system "echo 74 $(pwd)"
   end
 
   test do
