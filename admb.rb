40af985d85a13154908770127aef54f13a621fc9
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2022-08-14 18:06:45 +0900

    refactor

--- admb.rb
+++ admb.rb
@@ -1,30 +1,28 @@
-#
 class Admb < Formula
   desc "AD Model builder"
   homepage "http://admb-project.org"
   url "https://github.com/admb-project/admb/archive/refs/tags/admb-13.0.tar.gz"
   sha256 "d1e3f52baa7dee6c7d9eca2b3946c61e7f5468cf6c07307469162fc5a7acd310"
-  #head "https://github.com/admb-project/admb.git" , :branch => "issue157" 
-  head "https://github.com/admb-project/admb.git" 
+  license "BSD-3-Clause"
   revision 4
-  depends_on "flex"
+  head "https://github.com/admb-project/admb.git"
   keg_only "cs.h conflicts with cs.h from suitesparse"
-  license ""
+  depends_on "flex"
   def install
     # $ clang --version | grep version | sed "s/.*version \([0-9]*\.[0-9]*\).*/\1/"
     # 11.0
     # $ clang  -dM -E -x c   /dev/null |grep clang_major | awk -F " " '{ print $NF }'
-    # 11 
+    # 11
     # ENV.deparallelize   if your formula fails when building in parallel
     # separate make steps
-    # 
+    #
     # clang++-all:
-	  # $(MAKE) clang++-dist
-	  # $(MAKE) clang++-shared
-	  # $(MAKE) --directory=src CC=clang CXX=clang++ copy
+    # $(MAKE) clang++-dist
+    # $(MAKE) clang++-shared
+    # $(MAKE) --directory=src CC=clang CXX=clang++ copy
     # clang++-shared:
-	  # $(MAKE) --directory=src CC=clang CXX=clang++ SHARED=-shared shared
-	  # $(MAKE) --directory=contrib CC=clang CXX=clang++ SHARED=-shared shared
+    # $(MAKE) --directory=src CC=clang CXX=clang++ SHARED=-shared shared
+    # $(MAKE) --directory=contrib CC=clang CXX=clang++ SHARED=-shared shared
     # $(MAKE) clang++-dist
 
     system "make", "#{ENV.cxx}-contribs"
@@ -34,34 +32,31 @@ class Admb < Formula
     system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared", "shared"
     system "make", "--directory=contrib", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "SHARED=-shared", "shared"
     system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "copy"
-    # 
-    #clang++-install:
-	  # $(MAKE) --directory=src CC=clang CXX=clang++ install
-    #system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "install"
-    #
+    # clang++-install:
+    # $(MAKE) --directory=src CC=clang CXX=clang++ install
+    # system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "install"
     system "echo $(pwd)"
     system "find", "build/admb", "-type", "d", "-exec", "chmod", "-v", "755", "{}", "\;"
-	  #system "find", "build/admb", "-type", "f", "-exec", "chmod", "644", "{}", "\;"
     system "echo $(pwd)"
     system "find", "build/admb", "-type", "f", "-exec", "chmod", "-v", "644", "{}", "\;"
     # cp -Rvf ../build/$(ADMB_VER) $(INSTALL_DIR)
     system "echo $(pwd)"
     # system ":chdir", "build/admb", "find",  ".", "-type", "d", "-exec", "install", "-v", "-d", "#{prefix}/{}", "\\;"
-    #spawn "find . -type d -exec install -v -d #{prefix}/{} \\; " , :chdir=>"build/admb"
-    #Dir.chdir "build/admb" do
+    # spawn "find . -type d -exec install -v -d #{prefix}/{} \\; " , :chdir=>"build/admb"
+    # Dir.chdir "build/admb" do
     #  system "find . -type d -exec install -v -d #{prefix}/{} \\; "
     #  system "find . -type f -exec install -vC {} #{prefix}/{} \\; "
-    #end
+    # end
     bin.install Dir["build/admb/bin/*"]
     lib.install Dir["build/admb/lib/*"]
     include.install Dir["build/admb/include/*"]
-    prefix.install  Dir["build/admb/contrib"]
-    prefix.install Dir["build/admb/examples"]
-    prefix.install Dir["build/admb/scripts"]
-    prefix.install Dir["build/admb/src"]
-    prefix.install Dir["build/admb/tests"]
-    prefix.install Dir["build/admb/utilities"]
-    prefix.install "Makefile" 
+    prefix.install "build/admb/contrib"
+    prefix.install "build/admb/examples"
+    prefix.install "build/admb/scripts"
+    prefix.install "build/admb/src"
+    prefix.install "build/admb/tests"
+    prefix.install "build/admb/utilities"
+    prefix.install "Makefile"
     prefix.install Dir["*.txt"]
     prefix.install Dir["*.md"]
   end
@@ -76,11 +71,11 @@ class Admb < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    #system "false"
-    #system "make", "#{ENV.cxx}-test"
-    system "make", "--directory=#{prefix}/tests", "all", "CXX=#{ENV.cxx}"
-    Dir.chdir "#{prefix}" do
-      system "make", test
-    end 
+    system "false"
+    # system "make", "#{ENV.cxx}-test"
+    # system "make", "--directory=#{prefix}/tests", "all", "CXX=#{ENV.cxx}"
+    # Dir.chdir "#{prefix}" do
+    #  system "make", test
+    # end
   end
 end
