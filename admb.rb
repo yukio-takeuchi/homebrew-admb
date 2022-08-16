cd6b2b5b3ac06f6d5ba841b48a109796d8f488a7
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2022-08-17 06:31:53 +0900

    test refactor

--- admb.rb
+++ admb.rb
@@ -112,8 +112,12 @@ class Admb < Formula
       # $(MAKE) g++-core
       # $(MAKE) g++-contribs
       # g++-core:
+      ENV["ADCXXFLAGS"] = "-fPIC  -O3 -std=c++17 -I../build/admb/include"
+      #system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all", \
+      #"ADCXXFLAGS= -fPIC  -O3 -std=c++17 -I../build/admb/include"
       system "make", "--directory=src", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all", \
-      "ADCXXFLAGS= -fPIC  -O3 -std=c++17 -I../build/admb/include"
+      "ADCXXFLAGS= #{ENV['ADCXXFLAGS']}"
+
       # g++-contribs: g++-core
       system "make", "--directory=contrib", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "all", \
       "ADCXXFLAGS= -fPIC  -O3 -std=c++17 -I../build/admb/include"
